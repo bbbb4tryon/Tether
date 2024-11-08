9005e285b9a1c42a42b9686d69939ad3a97f99f2
bbb4tryon <b.t.tryon@gmail.com>
2024-11-08 14:00:46 -0700

    Initial commit: New Tether project with SwiftUI, Swift 6, and protocol-oriented architecture

---
+++ Tether/TetherApp.swift
@@ -0,0 +1,17 @@
+//
+//  TetherApp.swift
+//  Tether
+//
+//  Created by Benjamin Tryon on 11/8/24.
+//
+
+import SwiftUI
+
+@main
+struct TetherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
